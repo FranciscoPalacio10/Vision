08fa8151b814b8a99e55eebd26e381b7f80d83a8
FranciscoPalacio10 <franciscoadrianpalacio@gmail.com>
2021-11-13 13:45:59 -0300

    first commit

---
+++ app/src/main/java/com/google/mlkit/vision/demo/java/posedetector/PartOfBodysAngles.java
@@ -0,0 +1,22 @@
+package com.google.mlkit.vision.demo.java.posedetector;
+
+public class PartOfBodysAngles {
+    public String parte;
+    public double angulo;
+
+    public String getParte() {
+        return parte;
+    }
+
+    public void setParte(String parte) {
+        this.parte = parte;
+    }
+
+    public double getAngulo() {
+        return angulo;
+    }
+
+    public void setAngulo(double angulo) {
+        this.angulo = angulo;
+    }
+}
